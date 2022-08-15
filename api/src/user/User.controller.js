df1c469f25fda7bc5641ceb6fcead36cef9ffa81
mate444 <mateoeo23@gmail.com>
2022-08-15 18:26:01 -0300

    feature/controllers - Added User Login controller

--- api/src/user/User.controller.js
+++ api/src/user/User.controller.js
@@ -14,4 +14,19 @@ router.post('/register', async (req, res, next) => {
   }
 });
 
+router.post('/login', async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const foundUser = await UserProvider.CheckUser(email);
+    if (foundUser === 'User not found') return res.status(400).send({ message: foundUser });
+    const passwordMatchs = await bcrypt.compare(password, foundUser.password);
+    if (!passwordMatchs) {
+      return res.status(400).send({ message: 'Password do not match' });
+    }
+    res.send({ user: foundUser });
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
